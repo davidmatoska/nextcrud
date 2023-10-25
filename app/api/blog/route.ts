e61eac8e004cb23a08ff47cb33b765e52e856665
David Matoska <david.matoska@gmail.com>
2023-10-25 22:10:36 +0200

    commit

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -14,6 +14,7 @@ export const GET = async (req: Request, res: NextResponse) => {
   try {
     await main();
     const posts = await prisma.post.findMany();
+    return NextResponse.json ({message:"success", posts}, {status: 200});
   } catch (err) {
     return NextResponse.json({message:"Error",err}, {status:500});
   }
