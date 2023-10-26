520651b0b040280360ede31e5f460cb5354db0c8
David Matoska <david.matoska@gmail.com>
2023-10-26 21:48:47 +0200

    commit

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -2,24 +2,35 @@ import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 
 async function main() {
-    try {
-        await prisma.$connect();
-    } catch (err) {
-        return Error;
-        ("database connection failed");
-    }
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error;
+    ("database connection failed");
+  }
 }
 
 export const GET = async (req: Request, res: NextResponse) => {
   try {
     await main();
     const posts = await prisma.post.findMany();
-    return NextResponse.json ({message:"success", posts}, {status: 200});
+    return NextResponse.json({ message: "success", posts }, { status: 200 });
   } catch (err) {
-    return NextResponse.json({message:"Error",err}, {status:500});
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
   }
 };
 
 export const POST = async (req: Request, res: NextResponse) => {
-  console.log("POST");
+  try {
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.create({ data: { description, title } });
+    return NextResponse.json({ message: "success", post}, {status: 201});
+  } catch (err) {
+    return NextResponse.json({ message: "error", err}, {status: 500});
+  } finally {
+    await prisma.$disconnect();
+  }
 };
