9c235c272f82246f05a89c490b9fa6810140c7c9
David Matoska <david.matoska@gmail.com>
2023-10-27 18:17:18 +0200

    commit

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -5,15 +5,44 @@ import prisma from "@/prisma";
 export const GET = async (req: Request, res: NextResponse) => {
   try {
     const id = req.url.split("/blog/")[1];
-    await main;
+    await main();
     const post = await prisma.post.findFirst({ where: { id } });
     if (!post)
-      return NextResponse.json({ message: "not found"}, { status: 500 });
+      return NextResponse.json({ message: "Not Found" }, { status: 404 });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ message: "error", error }, { status: 500 });
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
   }
 };
 
-export const PUT = async (req: Request, res: NextResponse) => {};
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id = req.url.split("/blog/")[1];
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
 
-export const DELETE = async (req: Request, res: NextResponse) => {};
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id = req.url.split("/blog/")[1];
+    await main();
+    const post = await prisma.post.delete({ where: { id } });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -5,8 +5,7 @@ export async function main() {
   try {
     await prisma.$connect();
   } catch (err) {
-    return Error;
-    ("database connection failed");
+    return Error("Database Connection Unsuccessull");
   }
 }
 
@@ -14,7 +13,7 @@ export const GET = async (req: Request, res: NextResponse) => {
   try {
     await main();
     const posts = await prisma.post.findMany();
-    return NextResponse.json({ message: "success", posts }, { status: 200 });
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   } finally {
@@ -27,9 +26,9 @@ export const POST = async (req: Request, res: NextResponse) => {
     const { title, description } = await req.json();
     await main();
     const post = await prisma.post.create({ data: { description, title } });
-    return NextResponse.json({ message: "success", post}, {status: 201});
+    return NextResponse.json({ message: "Success", post }, { status: 201 });
   } catch (err) {
-    return NextResponse.json({ message: "error", err}, {status: 500});
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
